17d34819133ad77052dedc96ec8abc2b122c634b
phy-lei <765373325@qq.com>
2024-05-27 19:36:02 +0800

    feat: nav基本样式

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,14 +1,30 @@
+import { onMount, onCleanup } from 'solid-js';
 import ExpandButton from '../expand-button';
 const Nav = () => {
+  onMount(() => {
+    const scrollEvent = () => {
+      // @TODO 添加nav border 及背景色
+      console.log(
+        '%c [ xxx ]',
+        'font-size:13px; background:pink; color:#bf2c9f;',
+        123
+      );
+    };
+    window.addEventListener('scroll', scrollEvent);
+    onCleanup(() => {
+      window.removeEventListener('scroll', scrollEvent);
+    });
+  });
+
   return (
-    <nav class="h-72px">
-      <div class="flex items-center justify-between">
+    <nav class="fixed w-full h-72px bg-red">
+      <div class="h-full flex items-center justify-between">
         <div class="flex">
           <h1>Volt+</h1>
           <h1>icon</h1>
         </div>
-        <div class="flex">
-          <div>
+        <div class="flex items-center">
+          <div class="flex gap-4">
             <a href="">光伏介绍</a>
             <a href="">工商业解决方案</a>
             <a href="">成功案例</a>
