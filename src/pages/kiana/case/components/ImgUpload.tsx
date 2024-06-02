b24bc9ec342b9db98baace4881233331440ffe14
phy-lei <765373325@qq.com>
2024-06-02 16:58:06 +0800

    feat: 上传功能

---
+++ src/pages/kiana/case/components/ImgUpload.tsx
@@ -0,0 +1,57 @@
+import * as fileUpload from '@zag-js/file-upload';
+import { normalizeProps, useMachine } from '@zag-js/solid';
+import { createSignal, createUniqueId, createMemo, Index } from 'solid-js';
+import Button from '@/components/button';
+
+export default function ImgUpload() {
+  const [previewUrl, setPreviewUrl] = createSignal<string>('');
+  const [state, send] = useMachine(
+    fileUpload.machine({
+      id: createUniqueId(),
+      maxFiles: 1,
+      accept: 'image/*',
+      onFileChange(details) {
+        const reader = new FileReader();
+        reader.onload = (event) => {
+          const image = event.target.result;
+          setPreviewUrl(image as string);
+        };
+        reader.readAsDataURL(details.acceptedFiles[0]);
+      },
+    })
+  );
+
+  const api = createMemo(() => fileUpload.connect(state, send, normalizeProps));
+
+  return (
+    <div {...api().rootProps} class="flex items-center">
+      <div {...api().dropzoneProps}>
+        <input {...api().hiddenInputProps} />
+      </div>
+
+      <div classList={{ hidden: api().acceptedFiles.length > 0 }}>
+        <Button {...api().triggerProps} class="mr-2">
+          <i class="i-carbon:add-filled mr-1"></i>
+          封面
+        </Button>
+      </div>
+
+      <ul {...api().itemGroupProps} class="h-120px">
+        <Index each={api().acceptedFiles}>
+          {(file) => (
+            <li
+              {...api().getItemProps({ file: file() })}
+              class="w-120px h-120px border border-[var(--main-color)] rounded relative flex items-center justify-center"
+            >
+              <img src={previewUrl()} class="w-100px h-100px" alt="" />
+              <i
+                class="i-carbon:close cursor-pointer absolute right-1 top-1"
+                {...api().getItemDeleteTriggerProps({ file: file() })}
+              ></i>
+            </li>
+          )}
+        </Index>
+      </ul>
+    </div>
+  );
+}
