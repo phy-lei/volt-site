c283857db51a9a74c2028f8dbc084a112fd9d926
phy-lei <765373325@qq.com>
2024-06-06 15:05:26 +0800

    chore

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,6 +26,7 @@ export default defineConfig({
     }),
   ],
   adapter: envAdapter(),
+  server: { port: 4321, host: true },
   vite: {
     ssr: {
       noExternal: process.env.OUTPUT ? false : true
