a318e503c55330beabf7d9b0a162d7ef2e1594dc
phy-lei <765373325@qq.com>
2024-07-14 00:03:14 +0800

    feat: 完成首页改版

--- unocss.config.ts
+++ unocss.config.ts
@@ -45,6 +45,8 @@ export default defineConfig({
       'text-accent-foreground': 'text-[hsl(var(--accent-foreground))]',
       'nav-link': 'flex h-10 w-max items-center justify-center gap-4 rounded-full px-4 py-2 transition-colors hover:bg-accent/.2 focus:bg-accent/.2 focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/.2 data-[state=open]:bg-accent/.2',
       'fade-trigger': 'transition-property-[transform,opacity] transition-duration-500 translate-y-[50px] opacity-0',
+      'text-theme': 'color-[var(--main-color)]',
+      'location-center': 'top-50% left-50% translate-x--50% translate-y--50%',
     },
     [/^bg-accent\/(.*)$/, ([, c]) => `bg-[hsl(var(--accent)/${c})]`],
   ],
